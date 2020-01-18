d6d9a9abd1ee716e5ec5c56bf971b8711d865747
Raghav Sharma <raghavsyt@gmail.com>
2020-01-18 15:20:12 +0530

    refactored code refactored code, added error handling and logging

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-require('express-async-errors');
-const winston = require('winston');
-require('winston-mongodb');
-const config = require('config');
-const Joi = require('@hapi/joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middleware/error");
+const winston = require('winston');
 
-winston.add(winston.transports.File, {
-    filename: 'logfile.log'
-});
-winston.add(winston.transports.MongoDB, {
-    db: 'mongodb://localhost/vidflix',
-    level: 'error'
-});
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
 
-if (!config.get('jwtPrivateKey')) {
-    console.log('Error: jwtPrivateKey is not defined!');
-    process.exit(1);
-}
-
-// support parsing of application/json type post data
-app.use(bodyParser.json());
-
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-
-app.use(error);
-
-mongoose.connect('mongodb://localhost/vidflix', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('Connected to db...'))
-    .catch(err => console.error('Not connected to mongodb!'))
-
-
+throw new Error('Somethi..')
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+app.listen(PORT, () => winston.info(`Listening on port ${PORT}...`));
